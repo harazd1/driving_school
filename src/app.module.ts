3175d6b3b6ba205bf8f9d24396e1fb14c203de0d
Mykola <mykola.harazdii@gmail.com>
2023-03-25 15:12:02 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cadet } from './Cadets/cadet/cadet.model';
+import { CadetModule } from './Cadets/cadet/cadet.module';
+import { Fleet } from './Fleets/fleet.model';
+import { FleetModule } from './Fleets/fleet.module';
+import { Instructor } from './Instructors/instructor.model';
+import { InstructorModule } from './Instructors/instructor.module';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'password',
+      database:'driving_school',
+      entities:[Cadet, Instructor, Fleet],
+      synchronize: true,
+    }),
+     CadetModule,
+     InstructorModule,
+     FleetModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
